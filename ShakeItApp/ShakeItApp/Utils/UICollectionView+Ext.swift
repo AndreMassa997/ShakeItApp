edbb18c026a2d3d6f5b38b123bdf68913da4cf7c
Andrea Massari <a.massari@reply.it>
2024-05-18 17:50:03 +0200

    UIcollectionviewExtension implementation

--- ShakeItApp/ShakeItApp/Utils/UICollectionView+Ext.swift
+++ ShakeItApp/ShakeItApp/Utils/UICollectionView+Ext.swift
@@ -5,4 +5,19 @@
 //  Created by Andrea Massari on 18/05/24.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    // MARK: - Cells
+    final func register<T: UICollectionViewCell>(cellType: T.Type) where T: CellReusable {
+        self.register(cellType.self, forCellWithReuseIdentifier: cellType.reuseIdentifier)
+    }
+    
+    final func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath, cellType: T.Type = T.self) -> T where T: CellReusable {
+        let cell = self.dequeueReusableCell(withReuseIdentifier: cellType.reuseIdentifier, for: indexPath)
+        guard let typedCell = cell as? T else {
+            preconditionFailure("Failed to dequeue a cell with identifier \(cellType.reuseIdentifier) matching type \(cellType). Check that the reuseIdentifier is set properly in your XIB/Storyboard and that you registered the cell for reuse.")
+        }
+        return typedCell
+    }
+}
