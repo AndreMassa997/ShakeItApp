d76e40704a7308b12da5a83af7612679f6fe3dea
Andrea Massari <a.massari@reply.it>
2024-05-19 15:35:07 +0200

    Added cache for images

--- ShakeItApp/ShakeItApp/API/ImagesManager.swift
+++ ShakeItApp/ShakeItApp/API/ImagesManager.swift
@@ -8,7 +8,14 @@
 import Foundation
 
 final class ImagesManager: ImageProvider {
-    func fetchImage(from url: URL) async -> Result<Data, ErrorData> {        
+    private let cache: NSCache = NSCache<AnyObject, AnyObject>()
+    
+    func fetchImage(from url: URL) async -> Result<Data, ErrorData> {
+        if let imageFromCache = cache.object(forKey: url as AnyObject) as? Data {
+            print("🟠 Image Data cached from: \(url.absoluteString)")
+            return .success(imageFromCache)
+        }
+        
         print("🔵 URL request for image: \(url.absoluteString) at timestamp: \(Date().timeIntervalSince1970 * 1000)")
         
         guard let dataResponse = try? await URLSession.shared.data(from: url) else {
@@ -17,6 +24,7 @@ final class ImagesManager: ImageProvider {
         }
         
         print("🟢 Image Data Retrieved from: \(url.absoluteString)")
+        cache.setObject(dataResponse.0 as AnyObject, forKey: url as AnyObject)
         return .success(dataResponse.0)
     }
 }
