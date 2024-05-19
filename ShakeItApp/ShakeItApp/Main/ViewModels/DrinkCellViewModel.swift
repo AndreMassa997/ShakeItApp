2a7f9f7a647b79c7eab89620d9477842cbae2602
Andrea Massari <a.massari@reply.it>
2024-05-19 15:35:31 +0200

    Added load images when drinkCellViewModel is instantiated

--- ShakeItApp/ShakeItApp/Main/ViewModels/DrinkCellViewModel.swift
+++ ShakeItApp/ShakeItApp/Main/ViewModels/DrinkCellViewModel.swift
@@ -6,14 +6,31 @@
 //
 
 import Foundation
+import Combine
 
-final class DrinkCellViewModel {
+final class DrinkCellViewModel: ObservableObject {
     let drink: Drink
     let imageProvider: ImageProvider
+    
+    @Published var drinkImageData: Data?
+    var anyCancellables: Set<AnyCancellable> = Set()
     
     init(drink: Drink, imageProvider: ImageProvider) {
         self.drink = drink
         self.imageProvider = imageProvider
+        loadImage()
+    }
+    
+    func loadImage() {
+        Task {
+            let response = await imageProvider.fetchImage(from: drink.imageURL)
+            switch response {
+            case let .success(data):
+                self.drinkImageData = data
+            case .failure:
+                break
+            }
+        }
     }
     
     let backgroundColor: String = "#fdf9e6"
