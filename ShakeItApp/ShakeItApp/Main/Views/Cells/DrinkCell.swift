2c96c9bd94ed14f2138bef2ecac646dc87773f89
Andrea Massari <a.massari@reply.it>
2024-05-19 14:23:14 +0200

    Graphics fix on DrinkCell

--- ShakeItApp/ShakeItApp/Main/Views/Cells/DrinkCell.swift
+++ ShakeItApp/ShakeItApp/Main/Views/Cells/DrinkCell.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 final class DrinkCell: UITableViewCell, CellReusable {
+    private let viewContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     private let alcoholicTypeImageView: CircleImageView = {
         let iv = CircleImageView()
         iv.contentMode = .scaleAspectFill
@@ -18,14 +24,14 @@ final class DrinkCell: UITableViewCell, CellReusable {
     private let titleLabel: UILabel = {
         let lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        lbl.font = UIFont.systemFont(ofSize: 16, weight: .light)
         return lbl
     }()
     
     private let ingredientsLabel: UILabel = {
         let lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.font = UIFont.systemFont(ofSize: 14, weight: .thin)
+        lbl.font = UIFont.systemFont(ofSize: 12, weight: .thin)
         return lbl
     }()
     
@@ -42,6 +48,7 @@ final class DrinkCell: UITableViewCell, CellReusable {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
         self.addSubviews()
         self.setupLayout()
     }
@@ -49,39 +56,40 @@ final class DrinkCell: UITableViewCell, CellReusable {
     private func addSubviews() {
         labelsViewContainer.addArrangedSubview(titleLabel)
         labelsViewContainer.addArrangedSubview(ingredientsLabel)
-        self.contentView.addSubview(alcoholicTypeImageView)
+        self.viewContainer.addSubview(alcoholicTypeImageView)
+        self.viewContainer.addSubview(labelsViewContainer)
+        self.contentView.addSubview(viewContainer)
     }
     
     private func setupLayout() {
-        self.contentView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            contentView.leftAnchor.constraint(equalTo: leftAnchor, constant: 15),
-            contentView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
-            contentView.rightAnchor.constraint(equalTo: rightAnchor, constant: -15),
-            contentView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+            viewContainer.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 15),
+            viewContainer.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            viewContainer.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -15),
+            viewContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+            viewContainer.bottomAnchor.constraint(equalTo: alcoholicTypeImageView.bottomAnchor, constant: 5),
             
-            alcoholicTypeImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            alcoholicTypeImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
-            alcoholicTypeImageView.widthAnchor.constraint(equalToConstant: 40),
+            alcoholicTypeImageView.topAnchor.constraint(equalTo: viewContainer.topAnchor, constant: 10),
+            alcoholicTypeImageView.widthAnchor.constraint(equalToConstant: 50),
             alcoholicTypeImageView.heightAnchor.constraint(equalTo: alcoholicTypeImageView.widthAnchor),
-            alcoholicTypeImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10),
+            alcoholicTypeImageView.leftAnchor.constraint(equalTo: viewContainer.leftAnchor, constant: 10),
             
-            labelsViewContainer.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            labelsViewContainer.topAnchor.constraint(equalTo: viewContainer.topAnchor, constant: 15),
             labelsViewContainer.leftAnchor.constraint(equalTo: alcoholicTypeImageView.rightAnchor, constant: 10),
-            labelsViewContainer.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -5),
-            labelsViewContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            labelsViewContainer.rightAnchor.constraint(equalTo: viewContainer.rightAnchor, constant: -5),
+            labelsViewContainer.bottomAnchor.constraint(equalTo: viewContainer.bottomAnchor, constant: -15),
         ])
         
-        contentView.layer.cornerRadius = 20
-        contentView.layer.masksToBounds = false
-        contentView.layer.shadowColor = UIColor.black.cgColor
-        contentView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        contentView.layer.shadowRadius = 3
-        contentView.layer.shadowOpacity = 0.01
+        viewContainer.layer.cornerRadius = 20
+        viewContainer.layer.masksToBounds = false
+        viewContainer.layer.shadowColor = UIColor.black.cgColor
+        viewContainer.layer.shadowOffset = CGSize(width: 0, height: 0)
+        viewContainer.layer.shadowRadius = 3
+        viewContainer.layer.shadowOpacity = 0.01
     }
     
     func configure(with viewModel: DrinkCellViewModel) {
-        contentView.backgroundColor = UIColor(hex: viewModel.backgroundColor)
+        viewContainer.backgroundColor = UIColor(hex: viewModel.backgroundColor)
         titleLabel.text = viewModel.drink.name.capitalized
         ingredientsLabel.text = viewModel.ingredientsString
     }
