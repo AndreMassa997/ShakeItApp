58834b0e09bc344d12b27917d6303ccb54e4e390
Andrea Massari <a.massari@reply.it>
2024-05-22 23:35:08 +0200

    add MVVMTableViewController to avoid code duplication for tableview initialization

---
+++ ShakeItApp/ShakeItApp/Common/Architecture/MVVMTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MVVMTableViewController.swift
+//  ShakeItApp
+//
+//  Created by Andrea Massari on 22/05/24.
+//
+
+import UIKit
+
+class MVVMTableViewController<ViewModel: BaseViewModel>: MVVMViewController<ViewModel> {
+    let tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .plain)
+        tv.backgroundColor = .white
+        tv.separatorStyle = .none
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.showsVerticalScrollIndicator = false
+        tv.backgroundColor = .clear
+        if #available(iOS 15.0, *) {
+            tv.sectionHeaderTopPadding = 0
+        }
+        return tv
+    }()
+    
+    override func addSubviews() {
+        self.view.addSubview(tableView)
+    }
+    
+    override func setupLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
