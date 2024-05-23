bf1697dfec4a3ce03e6291f5659bd22bdc293c91
Andrea Massari <a.massari@reply.it>
2024-05-23 08:31:30 +0200

    Added common init for baseTableViewCell

--- ShakeItApp/ShakeItApp/Common/Architecture/BaseTableViewCell.swift
+++ ShakeItApp/ShakeItApp/Common/Architecture/BaseTableViewCell.swift
@@ -12,13 +12,18 @@ class BaseTableViewCell<ViewModel: BaseViewModel>: UITableViewCell, CellReusable
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addSubviews()
-        setupLayout()
-        setupUI()
+        commonInit()
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        addSubviews()
+        setupLayout()
+        setupUI()
     }
     
     func setupUI() {
