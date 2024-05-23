9b8d18a352534b681fd612cb6b70b96b3538a38b
Andrea Massari <a.massari@reply.it>
2024-05-23 08:52:55 +0200

    Added LabelCellViewModel for conformance

---
+++ ShakeItApp/ShakeItApp/Detail/ViewModel/LabelCellViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LabelCellViewModel.swift
+//  ShakeItApp
+//
+//  Created by Andrea Massari on 23/05/24.
+//
+
+import Foundation
+
+final class LabelCellViewModel: BaseViewModel {
+    let text: String
+    
+    init(text: String) {
+        self.text = text
+    }
+}
