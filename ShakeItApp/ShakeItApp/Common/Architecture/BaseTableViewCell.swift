d6a2aa86ccd4647740d091afec2f0b7a87fe1995
Andrea Massari <a.massari@reply.it>
2024-05-23 08:07:07 +0200

    Create basetableViewcell for cells

---
+++ ShakeItApp/ShakeItApp/Common/Architecture/BaseTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  BaseTableViewCell.swift
+//  ShakeItApp
+//
+//  Created by Andrea Massari on 22/05/24.
+//
+
+import UIKit
+
+class BaseTableViewCell<ViewModel: BaseViewModel>: UITableViewCell, CellReusable {
+    var viewModel: ViewModel!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubviews()
+        setupLayout()
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        selectionStyle = .none
+        backgroundColor = .clear
+    }
+    
+    //Mandatory override of this method to set view model
+    func configure(with viewModel: ViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    func bindProperties() {}
+    func addSubviews() {}
+    func setupLayout() {}
+    
+}
