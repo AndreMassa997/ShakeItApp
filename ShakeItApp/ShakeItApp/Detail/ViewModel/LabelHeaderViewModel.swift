9a0352f07e397b623cf94289112457064e0a0c2c
Andrea Massari <a.massari@reply.it>
2024-05-23 09:09:57 +0200

    Create BaseHeaderView for reuse with ViewModel + LabelHeaderViewModel

---
+++ ShakeItApp/ShakeItApp/Detail/ViewModel/LabelHeaderViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LabelHeaderViewModel.swift
+//  ShakeItApp
+//
+//  Created by Andrea Massari on 23/05/24.
+//
+
+import Foundation
+
+final class LabelHeaderViewModel: BaseViewModel {
+    let text: String
+    
+    init(text: String) {
+        self.text = text
+    }
+}
