4b31b83e0db3e576925e499066cddf8c5ac03ae0
Andrea Massari <a.massari@reply.it>
2024-05-23 11:00:49 +0200

    Implements BaseTableViewCell on NoItemsCells

--- ShakeItApp/ShakeItApp/Main/Views/Cells/NoItemsCell.swift
+++ ShakeItApp/ShakeItApp/Main/Views/Cells/NoItemsCell.swift
@@ -8,11 +8,10 @@
 import UIKit
 import Lottie
 
-final class NoItemsCell: UITableViewCell, CellReusable {
+final class NoItemsCell: BaseTableViewCell<BaseViewModel> {
     private let titleLabel: UILabel = {
         let lbl = UILabel()
         lbl.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
-        lbl.textColor = .palette.secondaryLabelColor
         lbl.translatesAutoresizingMaskIntoConstraints = false
         lbl.text = "MAIN.NO_DRINKS".localized
         lbl.numberOfLines = 0
@@ -29,18 +28,17 @@ final class NoItemsCell: UITableViewCell, CellReusable {
         return av
     }()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.textColor = .palette.secondaryLabelColor
+    }
+    
+    override func addSubviews() {
         self.contentView.addSubview(animatedView)
         self.contentView.addSubview(titleLabel)
-        setupLayout()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupLayout() {
+    override func setupLayout() {
         NSLayoutConstraint.activate([
             animatedView.widthAnchor.constraint(equalToConstant: 200),
             animatedView.heightAnchor.constraint(equalToConstant: 200),
